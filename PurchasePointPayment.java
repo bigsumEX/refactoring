d1dc44403e9862345053dd31d1dfb20dbb067748
大和蓮 <bigsum@yamatorennoMacBook-Air.local>
2023-07-05 01:08:04 +0900

    [Step3]条件チェックと値の代入

--- PurchasePointPayment.java
+++ PurchasePointPayment.java
@@ -6,17 +6,19 @@ class PurchasePointPayment {
     final PurchasePoint consumptionPoint; // 購入で消費するポイント
     final LocalDateTime paymentDateTime; // 購入日時
     PurchasePointPayment(final Customer customer, final Comic comic) {
-        if (customer.isEnabled()) {
+        if (!customer.isEnabled()) {
             throw new IllegalArgumentException("有効な購入者ではありません。");
         }   
         customerId = customer.id;
-        if (comic.isEnabled()) {
+        if (!comic.isEnabled()) {
             throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
         }
         customerId = customer.id;
         if (comic.currentPurchasePoint.amount <= customer.possessionPoint.amount) {
             throw new RuntimeException("所持ポイントが不足しています。");
         }
+        customerId = customer.id;
+        comicId = comic.id;
         consumptionPoint = comic.currentPurchasePoint;
         paymentDateTime = LocalDateTime.now();
     }
